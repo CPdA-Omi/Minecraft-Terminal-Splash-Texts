eb8d976f6ae3cd55694374e22873dd558526430e
CPdA-Omi <charlie.perrondarc@gmail.com>
2023-09-09 20:24:56 +0200

    script: improve speed by creating a file for messages number

--- splash/setSplashToRcFiles.sh
+++ splash/setSplashToRcFiles.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-MINECRAFT_SPLASH_TEXT_PATH=$(find -P ~ | grep splashText.sh | head -n 1)
+MINECRAFT_SPLASH_TEXT_PATH=$(find -P ~ | grep splashText.sh | head -n 1 | rev | cut -d '/' -f 2- | rev)
+
+if [ ! -e "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-total" ]; then
+	echo $(cat "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-texts" | wc -l) > "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-total"
+	echo "file \"minecraft-splash-total\" created at $MINECRAFT_SPLASH_TEXT_PATH"
+else #need update?
+	if [ $(cat "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-total") -ne $(cat "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-texts" | wc -l) ]; then
+		echo $(cat "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-texts" | wc -l) > "$MINECRAFT_SPLASH_TEXT_PATH/minecraft-splash-total"
+		echo "file \"minecraft-splash-total\" updated at $MINECRAFT_SPLASH_TEXT_PATH"
+	else
+		echo "file \"minecraft-splash-total\" already up to date (thank you)"
+	fi
+fi
 
 #bashrc
 if [ -e ~/.bashrc ]; then
-	if [ $(cat ~/.bashrc | grep ". ${MINECRAFT_SPLASH_TEXT_PATH}" | wc -l) -eq 0 ]; then
+	if [ $(cat ~/.bashrc | grep ". ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh" | wc -l) -eq 0 ]; then
 		echo "# Minecraft Terminal Splash Texts by CPdA-Omi
-if [ -f ${MINECRAFT_SPLASH_TEXT_PATH} ]; then
-	. ${MINECRAFT_SPLASH_TEXT_PATH}
+if [ -f ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh ]; then
+	. ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh
 fi
 # uncomment the following line if you want a new alias to display the message
-# alias mcst='. ${MINECRAFT_SPLASH_TEXT_PATH}'
+# alias mcst='. ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh'
 " >> ~/.bashrc
 		echo "~$USER/.bashrc updated with splashText.sh"
 	else
@@ -20,13 +32,13 @@ fi
 
 #zshrc
 if [ -e ~/.zshrc ]; then
-	if [ $(cat ~/.zshrc | grep ". ${MINECRAFT_SPLASH_TEXT_PATH}" | wc -l) -eq 0 ]; then
+	if [ $(cat ~/.zshrc | grep ". ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh" | wc -l) -eq 0 ]; then
 		echo "# Minecraft Terminal Splash Texts by CPdA-Omi
-if [ -f ${MINECRAFT_SPLASH_TEXT_PATH} ]; then
-	$(echo $MINECRAFT_SPLASH_TEXT_PATH | cut -d '/' -f-$(echo $MINECRAFT_SPLASH_TEXT_PATH | grep -o '/' | wc -l))/./splashText.sh
+if [ -f ${MINECRAFT_SPLASH_TEXT_PATH}/splashText.sh ]; then
+	${MINECRAFT_SPLASH_TEXT_PATH}/./splashText.sh
 fi
 # uncomment the following line if you want a new alias to display the message
-# alias mcst='$(echo $MINECRAFT_SPLASH_TEXT_PATH | cut -d '/' -f-$(echo $MINECRAFT_SPLASH_TEXT_PATH | grep -o '/' | wc -l))/./splashText.sh'
+# alias mcst='$(echo "$MINECRAFT_SPLASH_TEXT_PATH/splashText.sh")/./splashText.sh'
 " >> ~/.zshrc
 		echo "~$USER/.zshrc updated with splashText.sh"
 	else
